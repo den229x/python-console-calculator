745ef1d09dc35895a22ed7d603305d58be2d24c9
den229x <den229x@yandex.ru>
2021-09-07 23:28:08 +0300

    Optimized the code

--- 1.py
+++ 1.py
@@ -14,62 +14,41 @@ def choice(a, b, sign):
         except ArithmeticError:
             print ('ARITHMETIC ERROR: DIVISION BY ZERO')
             exit()
-    else:
-        return 'ERROR'
+
+    
+def err():
+    print('usage: [NAME_OF_PROGRAMM] [--help][-h] [--interactive][-i] first_num(integer) second_num(integer) sign[+, -, *, /]')
+    exit()
+    
         
 def main():
     parser = argparse.ArgumentParser(
         description="Calcurator", 
         usage='1.py [--help][-h] [--interactive][-i] first_num(integer) second_num(integer) sign[+, -, *, /]')
-    parser.add_argument('first_num', help='First Number', nargs='?', type=str, default=0)
-    parser.add_argument('second_num', help='Second Number', nargs='?', type=str, default=0)
-    parser.add_argument('sign', type=str, help='Sign', default='+', nargs='?')
+    parser.add_argument('first_num', help='First Integer Number', nargs='?', type=int, default=0)
+    parser.add_argument('second_num', help='Second Integer Number', nargs='?', type=int, default=0)
+    parser.add_argument('sign', type=str, help='Sign', default='+', nargs='?', choices=['+', '-', '*', '/'])
     parser.add_argument('-i', '--interactive', action='store_true', help='Enable interactive mode')
 
-
     num_of_arg = len(sys.argv)
-    if (num_of_arg == 1) | (num_of_arg > 4):
-        parser.print_usage()
-        exit()
-    
     namespace = parser.parse_args()
 
-    if (num_of_arg == 2) & (namespace.interactive == True):
+    if (num_of_arg == 2) & (namespace.interactive):
         values = input()
         values = values.split()
-
-        if len(values) != 3:
-            parser.print_usage()
-            exit()
         
         try:
             a = int(values[0])
             b = int(values[1])
             sign = values[2]
         except Exception:
-            parser.print_usage()
-            exit()
-
-        if choice(a, b, sign) == 'ERROR':
-            parser.print_usage()
-            exit()
+            err()
         
         print(choice(a, b, sign))
-    elif (num_of_arg == 4) & (namespace.interactive == False):
-        try:
-            int(namespace.first_num)
-            int(namespace.second_num)
-        except Exception:
-            parser.print_usage()
-            exit()
-
-        if choice(int(namespace.first_num), int(namespace.second_num), namespace.sign) == 'ERROR':
-            parser.print_usage()
-            exit()
-        
+    elif (num_of_arg == 4) & (namespace.interactive == False):   
         print(choice(int(namespace.first_num), int(namespace.second_num), namespace.sign))
     else:
-        parser.print_usage()
+        err()
 
 
 if __name__  =='__main__':
